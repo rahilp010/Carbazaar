fe20ac17227a660519c5da4fbe84efc34323e394
Rahil <rahilp704@gmail.com>
2023-05-15 09:10:34 +0530

    search appied to all
    Co-Authored-By: AnasMunshi <75517536+AnasMunshi2002@users.noreply.github.com>
    Co-Authored-By: rupin-gore <68654360+rupin-gore@users.noreply.github.com>
    Co-Authored-By: mmk-manavmistry <126792273+mmk-manavmistry@users.noreply.github.com>
    Co-Authored-By: Vadil06 <129254124+Vadil06@users.noreply.github.com>

---
+++ kia/search.js
@@ -0,0 +1,23 @@
+const Searchup = () => {
+    let filter = document.getElementById('myInput').value.toUpperCase();
+    let ul = document.getElementById('myUL');
+    let p = document.getElementsByTagName('p');
+    for (var i = 0; i < p.length; i++) {
+        let a = p[i].getElementsByTagName('a')[0];
+        let textvalue = a.innerHTML || a.textContent;
+        if (textvalue.toUpperCase().indexOf(filter) > -1) {
+            p[i].style.display = '';
+        } else {
+            p[i].style.display = 'none';
+        }
+    }
+}
+
+document.getElementById("myInput").onclick = function() {
+    document.getElementById("searchlist").style.display = "block";
+    document.getElementById("icon9").style.display = "block";
+}
+
+document.getElementById("icon9").onclick = function() {
+    document.getElementById("myInut").style.display = "none";
+}
